a1fe4074db97254e3dba1480baf570ed137bf31e
Mark Huang <hhzeos@gmail.com>
2021-06-12 11:24:04 +0800

    Update

--- web/src/components/PageDownload.js
+++ web/src/components/PageDownload.js
@@ -19,7 +19,8 @@ const PageDownload = () => {
             <div className={"container my-4 py-4 px-3 jumbotron shadow"} style={{minWidth: 400}}>
                 <p className="mb-5 alert alert-success">
                     <p><span className="fst-italic fw-bold me-2">News:</span> We are currently hosting a <a href="https://sutdcv.github.io/multi-modal-video-reasoning/">Multi-Modal Video Reasoning and Analyzing Competition</a> at <b>ICCV 2021</b> from 20 May 2021 to 5 July 2021, 
-                    the dataset access link will only be sent after the competition. To get early access to the dataset, you are encouraged to participate the above-mentioned competition!</p>
+                    the dataset access link will only be sent after the competition. To get early access to the dataset, you are encouraged to participate the aforementioned competition!</p>
+                    <p><span className="fst-italic fw-bold me-2">Note:</span> If you have gained access to the dataset from us previously, you are not eligible to participate in the competition.</p>
                     <p><span className="fst-italic fw-bold me-2">Note:</span> If you have registered for the competition, you DO NOT need to request the dataset here again.</p>
                 </p>
 
