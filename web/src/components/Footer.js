5c3bdd0b4e7523b1578f771cb4131badfcf3f122
Mark Huang <hhzeos@gmail.com>
2024-08-19 12:16:46 +0800

    Update

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div style={{ height: 70 }}></div>
             <hr className="mt-auto mx-5"></hr>
             <div className="container d-flex flex-row justify-content-center">
-                <p className="text-capitalize fw-light text-muted"><a href="https://github.com/sutdcv" className="text-reset">SUTD Computer Vision & Learning Group (VLG)</a> © 2023. All rights reserved.</p>
+                <p className="text-capitalize fw-light text-muted"><a href="https://github.com/sutdcv" className="text-reset">SUTD Computer Vision & Learning Group (VLG)</a> © 2021-2024. All rights reserved.</p>
             </div>
         </footer>
     )
--- web/src/components/PageDownload.js
+++ web/src/components/PageDownload.js
@@ -1,4 +1,3 @@
-import RequestForm from './RequestForm'
 
 
 const PageDownload = () => {
@@ -6,24 +5,37 @@ const PageDownload = () => {
         <div>
             <div className="jumbotron jumbotron-fluid py-5 bg-dark">
                 <div className="container">
-                    <h1 className="display-3 text-white text-center" id="dataset-download-request-form">Dataset Download Request Form</h1>
+                    <h1 className="display-3 text-white text-center" id="dataset-download-request-form">Dataset Download</h1>
                 </div>
-                <div className="mt-5 px-5 container text-light text-center">
-                    <p className="alert alert-secondary">
-                        To download the dataset (inc. <code>Annotations</code>, <code>Video Features</code>, and <code>Raw Videos</code>), you need to fill in the form below. 
+            </div>
+            <div className="container my-4 py-4 px-3 jumbotron jumbotron-fluid text-center" style={{ minWidth: 400 }}>
+
+                <h5 className="mb-4">Terms & Conditions</h5>
+
+                <div className="container text-center modal-dialog-centered align-items-center flex-column">
+                    <p className={"alert alert-primary text-start"} style={{ maxWidth: 500 }}>
+                        The Singapore University of Technology and Design (SUTD) provides access
+                        to the <em className="fw-bold">SUTD-TrafficQA Dataset</em> (referred to as "the Dataset" below)
+                        under the following conditions:
+                        <p></p>
+                        <ul>
+                            <li>The Dataset should only be used for non-commercial scientific research purposes. Any other use is strictly prohibited.</li>
+                            <li>Showing videos and images from the Dataset are only allowed in academic publications or presentations.</li>
+                            <li>The Dataset must not be shared or redistributed in part or full with any third-party individual or organization.</li>
+                            <li>The Dataset must not be altered to produce a new dataset without written consent from the authors.</li>
+                        </ul>
                     </p>
                 </div>
-            </div>
-            <div className={"container my-4 py-4 px-3 jumbotron shadow"} style={{minWidth: 400}}>
-                {/* <p className="mb-5 alert alert-success">
-                    <p><span className="fst-italic fw-bold me-2">News:</span> We are currently hosting a <a href="https://sutdcv.github.io/multi-modal-video-reasoning/">Multi-Modal Video Reasoning and Analyzing Competition</a> at <b>ICCV 2021</b> from 20 May 2021 to 5 July 2021, 
-                    the dataset access link will only be sent after the competition. To get early access to the dataset, you are encouraged to participate the aforementioned competition!</p>
-                    <p><span className="fst-italic fw-bold me-2">Note:</span> If you have gained access to the dataset from us previously, you are not eligible to participate in the competition.</p>
-                    <p><span className="fst-italic fw-bold me-2">Note:</span> If you have registered for the competition, you DO NOT need to request the dataset here again.</p>
-                </p> */}
 
-                <RequestForm />
+                <p className="text-center">By downloading the dataset, you agree to the terms and conditions above.</p>
+
+                <div className="container">
+                    <a href="https://bit.ly/SUTDTrafficQA-Google" className="button btn btn-outline-primary mt-3 mx-1" target="_blank" rel="noopener noreferrer">Google Drive Link</a>
+                    <a href="https://bit.ly/SUTDTrafficQA-OneDrive" className="button btn btn-outline-primary mt-3 mx-1" target="_blank" rel="noopener noreferrer">MS OneDrive Link</a>
+                </div>
             </div>
+
+
         </div>
     )
 }
